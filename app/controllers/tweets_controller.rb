b59000a1c4e9060d459bb82a28ee344624ab019f
kei-iwai-koji <iw.kj.16@gmail.com>
2019-01-14 14:14:02 +0900

    add menulist

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   def index
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -8,12 +9,19 @@ class TweetsController < ApplicationController
   end
 
   def create
+    tweet = Tweet.create(tweet_params)
+    redirect_to root_path
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
   end
 
 end
