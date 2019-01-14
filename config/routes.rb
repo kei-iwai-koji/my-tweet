e6a3f3905a412140919e22702e189095cc521e01
kei-iwai-koji <iw.kj.16@gmail.com>
2019-01-14 17:07:21 +0900

    add rooting-and-action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets
+  resources :tweets do
+    resources :comments, only: [:new, :create]
+  end
   resources :users
 end
