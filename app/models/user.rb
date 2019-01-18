756b6c57412d78e52b7be13f8af78bf79b9ae95e
kei-iwai-koji <iw.kj.16@gmail.com>
2019-01-18 13:25:56 +0900

    add associate-destoroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :tweets
-  has_many :comments
+  has_many :tweets, foreign_key: :user_id, dependent: :destroy
+  has_many :comments, foreign_key: :user_id, dependent: :destroy
   mount_uploader :avatar, ImageUploader
 end
